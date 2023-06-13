5a763bd6972cbbef5489d6de8130d838a25cc062
JoseMiracle <josephmiracle119@gmail.com>
2023-06-13 14:04:31 +0100

    string manipulations

---
+++ str-manipulation.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings" // package for string maninpulation
+)	
+
+
+func main(){
+	name := "Miracle";
+	
+	is_name_contain_yi := strings.Contains(name, "le");
+	fmt.Printf("%t\n", is_name_contain_yi);	
+
+	is_prefix_Mir := strings.HasPrefix(name, "Mir");
+	fmt.Printf("%t\n", is_prefix_Mir);
+
+	words := []string{"Golang", "is", "a", "good" , "programming" , "language."};
+	join_words := strings.Join(words, " ");
+	fmt.Printf("%s\n", join_words);
+
+	// more maninpulation will be added 
+}
